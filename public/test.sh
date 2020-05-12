fb6b2946d4dbed36f3351163fb00f833c5c17ffb
Labarre Gautier <gautier@labarre.me>
2020-05-12 16:54:21 +0200

    So far : Deleting an image removes it from app folder New app folder name with dot to make it invisible sudo prompt should work now Code cleaning

---
+++ public/test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Autor: Thiago Silva
+# Contact: thiagos.dasilva@gmail.com
+# URL: https://github.com/thiggy01/ubuntu-20.04-change-gdm-background
+# =================================================================== #
+
+# Check if script is run by root.
+if [ "$(id -u)" -ne 0 ] ; then
+    echo 'This script must be run as root.'
+    exit 1
+fi
+
+# Check if glib 2.0 development libraries are installed.
+if [ ! $(dpkg -s libglib2.0-dev-bin | grep Status | cut -d ' ' -f 4) \
+    == installed ]; then
+    echo 'Additional glib 2.0 libraries need to be installed.'
+    read -p 'Type y or Y to proceed. Any other key to exit: ' -n 1
+    if [[ "$REPLY" =~ ^[yY]$ ]]; then
+	apt install libglib2.0-dev-bin
+    else
+	echo
+	exit 1
+    fi
+fi
+
+echo "tout est bon"
+
+echo $1
+
+echo $name
+
+if [[ $(file --mime-type -b "$1") == image/*g ]]; then
+
+echo 'ok ok image push'
+fi
--- src/App.svelte
+++ src/App.svelte
@@ -1,58 +1,133 @@
 <script>
-	const fs = require("fs");
-	const {app} = require('electron').remote;
-	const home = app.getPath('home');
+    // Electron dependencies
+    const fs = require("fs");
+    const {exec} = require('child_process');
+    const sudo = require('sudo-prompt');
+    const {app} = require('electron').remote;
+    const home = app.getPath('home');
 
-	export let name;
-	import { onMount } from 'svelte';
+    // Svelte code
+    import {onMount} from 'svelte';
 
-	let backgroundFiles = [];
+    let backgroundList = [];
+    const appFolderName = '.gdm-background';
+    const appFolder = '/' + appFolderName + '/';
 
-	onMount(async () => {
-		fs.readdir(home + /background/, function(err, items) {
-			backgroundFiles = items;
+    onMount(async () => {
+        // Create app folder if it does not exist
+        const createFolder = exec('cd ' + home + '&& mkdir -p ' + appFolderName, (error, stdout, stderr) => {
+            if (error) {
+                console.log(error.stack);
+            }
+        });
+        createFolder.on('exit', () => getBackgroundList());
+    });
 
-			backgroundFiles.forEach(image => {
-				console.log(image.replace(/(\s+)/g, '\\$1') )
-			});
-		});
-	});
+    function getBackgroundList() {
+        fs.readdir(home + appFolder, (err, items) => backgroundList = items);
+    }
 
-	async function update(image) {
-		console.log(image);
-	}
+    function deleteImage(image) {
+        fs.unlinkSync(home + appFolder + image);
+        getBackgroundList()
+    }
+
+    function executeBackgroundChange(image) {
+        let script = __dirname + '/test.sh';
+        let imagePath = home + appFolder + image;
+        const options = { // Can't get it to work i think
+            name: 'Electron',
+            icns: '/Applications/Electron.app/Contents/Resources/Electron.icns', // (optional)
+        };
+
+        sudo.exec(script + ' ' + imagePath, options, (error, stdout, stderr) => {
+            if (error) throw error;
+            console.log('stdout: ' + stdout);
+        });
+    }
+
+    function reset() {
+        //TODO Launch default script with --restore option
+    }
+
+    function addImageToAppFolder(filename, ext) {
+        const mvToProperFolder = 'cd ' + home + appFolder;
+        const date = Date.now();
+
+        exec(mvToProperFolder + ' && cp ' + filename + ' ./' + date + ext, (error, stdout, stderr) => {
+            if (error) {
+                console.log(error.stack);
+                console.log('Error code: ' + error.code);
+                console.log('Signal received: ' + error.signal);
+            }
+            console.log(stdout);
+            console.log(stderr);
+        });
+    }
+
+    Dropzone.options.uploadWidget = {
+        paramName: 'file',
+        maxFilesize: 100, // MB
+        maxFiles: 1,
+        dictDefaultMessage: 'Drag an image here to upload, or click to select one (only works with JPG or PNG file)',
+        acceptedFiles: 'image/*',
+        init: function() {
+            this.on('addedfile', (file) => {
+                const filename = file.path.replace(/(\s+)/g, '\\$1');
+                let ext = '.jpg';
+                if (file.type === 'image/png') {
+                    ext = '.png';
+                }
+
+                addImageToAppFolder(filename, ext);
+            });
+        },
+        accept: function() {
+            this.removeAllFiles();
+            setTimeout(() => getBackgroundList(), 1500);
+        }
+    };
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-		{#each backgroundFiles as image}
-			<li><img src="{home}/background/{image}" on:click={update(image)} width="200" alt="test"></li>
-		{/each}
+    <form id="upload-widget" method="post" action="/" class="dropzone">
+        <div class="fallback">
+            <input name="file" type="file" multiple/>
+        </div>
+    </form>
 
-
-<!--	<button on:click={update}>update</button>-->
-
+    <button on:click={reset}>update</button>
+    {#each backgroundList as image}
+        <li>
+            <span on:click={deleteImage(image)}>DELETE</span>
+            <img src="{home}{appFolder}{image}" on:click={executeBackgroundChange(image)} width="800" alt="test">
+        </li>
+    {/each}
 </main>
 
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    li {
+        list-style-type: none;
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
